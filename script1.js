b6cf3280e0e312a4368f4ab292f1d7682e49d236
Sakthi kumar <142766263+sakthikumar-vk@users.noreply.github.com>
2023-09-26 15:50:55 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -4,8 +4,8 @@ var request = new XMLHttpRequest();
  request.send();
  request.onload=function(){
     var result = JSON.parse(request.response);
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].name.common);
+    for( i=0;i<result.length;i++){
+        console.log(result[i].flags);
     }
     
  }
