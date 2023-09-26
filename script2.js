9cb6b86efe50e1a15fb5d35eedc58b1b9f1af8d0
Sakthi kumar <142766263+sakthikumar-vk@users.noreply.github.com>
2023-09-26 16:15:33 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,10 +4,11 @@ var request = new XMLHttpRequest();
  request.onload=function(){
     var result = JSON.parse(request.response);
     for( i=0;i<result.length;i++){
-        console.log(result[i].name);
-        console.log(result[i].region);
-        console.log(result[i].subregion);
-        console.log(result[i].population);
+        console.log("Name:"+result[i].name.common);
+        console.log("Region:"+result[i].region);
+        console.log("Sub-region:"+result[i].subregion);
+        console.log("Population:"+ result[i].population);
+        console.log("----------------------");
     }
     
  }
